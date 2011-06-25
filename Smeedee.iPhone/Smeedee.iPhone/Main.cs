190583fee4b5055e2c9957976dc2bfcbbbb30a82
Alex York <alex-york@hotmail.co.uk>
2011-06-25 15:55:28 +0200

    Refactored the main entry point of the app to use shorter method names, and also introduced a place where global dependencies are configured. This will make it easier for us to switch dependencies between real and fake ones.

--- Smeedee.iPhone/Smeedee.iPhone/Main.cs
+++ Smeedee.iPhone/Smeedee.iPhone/Main.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using Smeedee.Model;
+using Smeedee.Services;
 
 namespace Smeedee.iPhone
 {
@@ -23,15 +24,31 @@ namespace Smeedee.iPhone
 		// This method is invoked when the application has loaded its UI and its ready to run
 		public override bool FinishedLaunching(UIApplication uiApp, NSDictionary options)
 		{
+			ConfigureDependencies();
+			RegisterAllSupportedWidgets();
+			AddMainTabBarToMenu();
+			
+			window.MakeKeyAndVisible();
+			return true;
+		}
+		
+		private void ConfigureDependencies()
+		{
+			//SmeedeeApp.SmeedeeService = new SmeedeeHttpService(); // Production
+			SmeedeeApp.SmeedeeService = new SmeedeeFakeService(); // Developing/offline
+		}
+		
+		private void RegisterAllSupportedWidgets()
+		{
 			app.RegisterAvailableWidgets(new [] {
 				typeof(SmeedeeWelcomeScreen),
 				typeof(TopCommitersScreen)
 			});
-			
+		}
+		
+		private void AddMainTabBarToMenu()
+		{
 			window.AddSubview(tabBarController.View);
-
-			window.MakeKeyAndVisible();
-			return true;
 		}
 	}
 }
