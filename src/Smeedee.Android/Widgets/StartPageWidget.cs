f118adde27119ec194e47096bb2f4e9467dfdce1
Borge Rodsjo <borge.rodsjo@gmail.com>
2011-07-05 14:38:20 +0200

    Added the start page widget

---
+++ src/Smeedee.Android/Widgets/StartPageWidget.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Smeedee.Android.Widgets
+{
+    public class StartPageWidget : LinearLayout, IWidget
+    {
+        private Context context;
+
+        public StartPageWidget(Context context) :
+            base(context)
+        {
+            this.context = context;
+            Initialize();
+        }
+
+        public StartPageWidget(Context context, IAttributeSet attrs) :
+            base(context, attrs)
+        {
+            this.context = context;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var smeedeeIcon = new ImageView(context);
+            smeedeeIcon.SetImageResource(Resource.Drawable.Icon);
+            AddView(smeedeeIcon);
+        }
+    }
+}
