69c369765a2ee005b653b1b0561be6225c4fa91d
Lars Eidnes <larseidnes@gmail.com>
2011-07-05 14:31:32 +0200

    Added integration test project, with test for the image service

--- src/Smeedee.IntegrationTests/ImageServiceTests.cs
+++ src/Smeedee.IntegrationTests/ImageServiceTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +13,29 @@ namespace Smeedee.IntegrationTests
     [TestFixture]
     public class ImageServiceTests
     {
-        [Test]
-        public void Should_respond_with_a_byte_array_when_GetImage_is_called()
+        private byte[] GetImageBytesFromDagbladet()
         {
             var imageService = new ImageService(new NoBackgroundWorker());
             byte[] recievedBytes = null;
             imageService.GetImage(new Uri("http://www.dagbladet.no/favicon.ico"),
                                   bytes => recievedBytes = bytes);
-            Assert.IsNotNull(recievedBytes);
-            Assert.AreNotEqual(0, recievedBytes.Length);
+            return recievedBytes;
+        }
+
+        [Test]
+        public void Should_respond_with_a_byte_array_when_GetImage_is_called()
+        {
+            byte[] bytes = GetImageBytesFromDagbladet();
+            Assert.IsNotNull(bytes);
+            Assert.AreNotEqual(0, bytes.Length);
+        }
+
+        [Test]
+        public void Recieved_byte_array_should_be_parseable_as_an_image()
+        {
+            byte[] bytes = GetImageBytesFromDagbladet();
+            var img = Image.FromStream(new MemoryStream(bytes));
+            Assert.AreNotEqual(0, img.Height);
         }
     }
 }
