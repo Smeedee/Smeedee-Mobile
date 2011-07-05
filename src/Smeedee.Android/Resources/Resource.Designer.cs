4059121632a6459024da0e3dfbda129e821e0df1
Lars K. Melhus <lars.kirkholt.melhus@gmail.com>
2011-07-05 16:31:19 +0200

    Including autogenerated

--- src/Smeedee.Android/Resources/Resource.Designer.cs
+++ src/Smeedee.Android/Resources/Resource.Designer.cs
@@ -40,8 +40,8 @@ namespace Smeedee.Android
 			// aapt resource value: 0x7f060004
 			public const int BtnEnabledWidgets = 2131099652;
 			
-			// aapt resource value: 0x7f06000d
-			public const int BtnGlobalSettings = 2131099661;
+			// aapt resource value: 0x7f060010
+			public const int BtnGlobalSettings = 2131099664;
 			
 			// aapt resource value: 0x7f060008
 			public const int BtnNext = 2131099656;
@@ -52,8 +52,8 @@ namespace Smeedee.Android
 			// aapt resource value: 0x7f060005
 			public const int BtnServerSettings = 2131099653;
 			
-			// aapt resource value: 0x7f06000c
-			public const int BtnWidgetSettings = 2131099660;
+			// aapt resource value: 0x7f06000f
+			public const int BtnWidgetSettings = 2131099663;
 			
 			// aapt resource value: 0x7f060000
 			public const int EnabledWidgetsTextView = 2131099648;
@@ -79,14 +79,14 @@ namespace Smeedee.Android
 			// aapt resource value: 0x7f060006
 			public const int TopBanner = 2131099654;
 			
-			// aapt resource value: 0x7f06000b
-			public const int WorkingDaysLeftLayout = 2131099659;
-			
 			// aapt resource value: 0x7f06000c
-			public const int WorkingDaysLeftNumber = 2131099660;
+			public const int WorkingDaysLeftLayout = 2131099660;
 			
 			// aapt resource value: 0x7f06000d
-			public const int WorkingDaysLeftText = 2131099661;
+			public const int WorkingDaysLeftNumber = 2131099661;
+			
+			// aapt resource value: 0x7f06000e
+			public const int WorkingDaysLeftText = 2131099662;
 			
 			private Id()
 			{
@@ -113,6 +113,9 @@ namespace Smeedee.Android
 			
 			// aapt resource value: 0x7f030005
 			public const int StartPageWidgetLayout = 2130903045;
+			
+			// aapt resource value: 0x7f030006
+			public const int WorkingDaysLeftWidget = 2130903046;
 			
 			private Layout()
 			{
