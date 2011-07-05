711670841c0876d377110170cd0589c50fb9b633
Dag Olav Prestegarden <dagolav@prestegarden.com>
2011-07-05 09:20:16 +0200

    Starting work on ServiceLocator, first test passing

---
+++ src/Smeedee.UnitTests/Services/ServiceLocatorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Smeedee.Services;
+
+namespace Smeedee.UnitTests.Services
+{
+    [TestFixture]
+    public class ServiceLocatorTests
+    {
+        [Test]
+        public void Should_be_able_to_bind_class_implementing_the_said_interface()
+        {
+            var locator = new ServiceLocator();
+            locator.Bind<ITestInterface>(new TestInterfaceImpl());
+        }
+    }
+
+    public class TestInterfaceImpl : ITestInterface
+    {
+    }
+
+    public interface ITestInterface
+    {
+    }
+}
---
+++ src/Smeedee/Services/ServiceLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smeedee.Services
+{
+    public class ServiceLocator
+    {
+        private Object store;
+
+        public void Bind<T>(T arg)
+        {
+            store = arg;
+        }
+    }
+}
