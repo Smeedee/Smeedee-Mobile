17efb021c81a0148ed1250922ffa111c8e91dfac
Lars K. Melhus <lars.kirkholt.melhus@gmail.com>
2011-07-05 14:00:12 +0200

    Resolving merge errors from previous commit

--- src/Smeedee.Android/Resources/Resource.Designer.cs
+++ src/Smeedee.Android/Resources/Resource.Designer.cs
@@ -37,38 +37,44 @@ namespace Smeedee.Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060003
-			public const int BtnEnabledWidgets = 2131099651;
-			
-			// aapt resource value: 0x7f06000a
-			public const int BtnGlobalSettings = 2131099658;
-			
-			// aapt resource value: 0x7f060007
-			public const int BtnNext = 2131099655;
-			
-			// aapt resource value: 0x7f060006
-			public const int BtnPrev = 2131099654;
-			
 			// aapt resource value: 0x7f060004
-			public const int BtnServerSettings = 2131099652;
+			public const int BtnEnabledWidgets = 2131099652;
 			
-			// aapt resource value: 0x7f060009
-			public const int BtnWidgetSettings = 2131099657;
+			// aapt resource value: 0x7f06000c
+			public const int BtnGlobalSettings = 2131099660;
 			
 			// aapt resource value: 0x7f060008
-			public const int Flipper = 2131099656;
+			public const int BtnNext = 2131099656;
 			
-			// aapt resource value: 0x7f060002
-			public const int GlobalSettingsHeadline = 2131099650;
-			
-			// aapt resource value: 0x7f060001
-			public const int GlobalSettingsTopBanner = 2131099649;
-			
-			// aapt resource value: 0x7f060000
-			public const int RootLayout = 2131099648;
+			// aapt resource value: 0x7f060007
+			public const int BtnPrev = 2131099655;
 			
 			// aapt resource value: 0x7f060005
-			public const int TopBanner = 2131099653;
+			public const int BtnServerSettings = 2131099653;
+			
+			// aapt resource value: 0x7f06000b
+			public const int BtnWidgetSettings = 2131099659;
+			
+			// aapt resource value: 0x7f060000
+			public const int EnabledWidgetsTextView = 2131099648;
+			
+			// aapt resource value: 0x7f060009
+			public const int Flipper = 2131099657;
+			
+			// aapt resource value: 0x7f060003
+			public const int GlobalSettingsHeadline = 2131099651;
+			
+			// aapt resource value: 0x7f060002
+			public const int GlobalSettingsTopBanner = 2131099650;
+			
+			// aapt resource value: 0x7f060001
+			public const int RootLayout = 2131099649;
+			
+			// aapt resource value: 0x7f06000a
+			public const int ServerSettingsTextView = 2131099658;
+			
+			// aapt resource value: 0x7f060006
+			public const int TopBanner = 2131099654;
 			
 			private Id()
 			{
@@ -79,13 +85,16 @@ namespace Smeedee.Android
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int GlobalSettings = 2130903040;
+			public const int EnabledWidgets = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int GlobalSettingsList = 2130903041;
+			public const int GlobalSettings = 2130903041;
 			
 			// aapt resource value: 0x7f030002
 			public const int Main = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int ServerSettings = 2130903043;
 			
 			private Layout()
 			{
@@ -116,7 +125,10 @@ namespace Smeedee.Android
 			public const int BtnServerSettings = 2130968581;
 			
 			// aapt resource value: 0x7f040008
-			public const int FooWidgetText = 2130968584;
+			public const int EnabledWidgets = 2130968584;
+			
+			// aapt resource value: 0x7f04000a
+			public const int FooWidgetText = 2130968586;
 			
 			// aapt resource value: 0x7f040003
 			public const int GlobalSettings = 2130968579;
@@ -128,7 +140,10 @@ namespace Smeedee.Android
 			public const int Prev = 2130968577;
 			
 			// aapt resource value: 0x7f040007
-			public const int TestWidgetText = 2130968583;
+			public const int ServerSettings = 2130968583;
+			
+			// aapt resource value: 0x7f040009
+			public const int TestWidgetText = 2130968585;
 			
 			// aapt resource value: 0x7f040004
 			public const int WidgetSettings = 2130968580;
