d9ceefa42dae5e778bab6e279df76558c4bbad6e
Dag Olav Prestegarden <dagolav@prestegarden.com>
2011-07-06 13:47:22 +0200

    UI pretty much done

--- src/Smeedee.Android/Resources/Resource.Designer.cs
+++ src/Smeedee.Android/Resources/Resource.Designer.cs
@@ -37,77 +37,74 @@ namespace Smeedee.Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f06000c
-			public const int BtnEnabledWidgets = 2131099660;
-			
-			// aapt resource value: 0x7f060018
-			public const int BtnGlobalSettings = 2131099672;
-			
-			// aapt resource value: 0x7f060011
-			public const int BtnNext = 2131099665;
-			
-			// aapt resource value: 0x7f060010
-			public const int BtnPrev = 2131099664;
-			
-			// aapt resource value: 0x7f06000d
-			public const int BtnServerSettings = 2131099661;
+			// aapt resource value: 0x7f06000b
+			public const int BtnEnabledWidgets = 2131099659;
 			
 			// aapt resource value: 0x7f060017
-			public const int BtnWidgetSettings = 2131099671;
+			public const int BtnGlobalSettings = 2131099671;
+			
+			// aapt resource value: 0x7f060010
+			public const int BtnNext = 2131099664;
+			
+			// aapt resource value: 0x7f06000f
+			public const int BtnPrev = 2131099663;
+			
+			// aapt resource value: 0x7f06000c
+			public const int BtnServerSettings = 2131099660;
+			
+			// aapt resource value: 0x7f060016
+			public const int BtnWidgetSettings = 2131099670;
 			
 			// aapt resource value: 0x7f060000
 			public const int BuildStatusLayout = 2131099648;
 			
-			// aapt resource value: 0x7f060008
-			public const int EnabledWidgetsTextView = 2131099656;
+			// aapt resource value: 0x7f060007
+			public const int EnabledWidgetsTextView = 2131099655;
 			
-			// aapt resource value: 0x7f060012
-			public const int Flipper = 2131099666;
-			
-			// aapt resource value: 0x7f06000b
-			public const int GlobalSettingsHeadline = 2131099659;
+			// aapt resource value: 0x7f060011
+			public const int Flipper = 2131099665;
 			
 			// aapt resource value: 0x7f06000a
-			public const int GlobalSettingsTopBanner = 2131099658;
-			
-			// aapt resource value: 0x7f06000e
-			public const int LoginScreen = 2131099662;
+			public const int GlobalSettingsHeadline = 2131099658;
 			
 			// aapt resource value: 0x7f060009
-			public const int RootLayout = 2131099657;
+			public const int GlobalSettingsTopBanner = 2131099657;
 			
-			// aapt resource value: 0x7f060013
-			public const int ServerSettingsScreenTextView = 2131099667;
+			// aapt resource value: 0x7f06000d
+			public const int LoginScreen = 2131099661;
+			
+			// aapt resource value: 0x7f060008
+			public const int RootLayout = 2131099656;
+			
+			// aapt resource value: 0x7f060012
+			public const int ServerSettingsScreenTextView = 2131099666;
 			
 			// aapt resource value: 0x7f060003
 			public const int SmeedeeIcon = 2131099651;
 			
-			// aapt resource value: 0x7f060014
-			public const int SmeedeeStartPageTextView = 2131099668;
+			// aapt resource value: 0x7f060013
+			public const int SmeedeeStartPageTextView = 2131099667;
 			
-			// aapt resource value: 0x7f06000f
-			public const int TopBanner = 2131099663;
+			// aapt resource value: 0x7f06000e
+			public const int TopBanner = 2131099662;
+			
+			// aapt resource value: 0x7f060014
+			public const int WorkingDaysLeftNumber = 2131099668;
 			
 			// aapt resource value: 0x7f060015
-			public const int WorkingDaysLeftNumber = 2131099669;
-			
-			// aapt resource value: 0x7f060016
-			public const int WorkingDaysLeftText = 2131099670;
+			public const int WorkingDaysLeftText = 2131099669;
 			
 			// aapt resource value: 0x7f060002
 			public const int build_list = 2131099650;
 			
 			// aapt resource value: 0x7f060004
-			public const int build_text_info = 2131099652;
+			public const int item2 = 2131099652;
 			
 			// aapt resource value: 0x7f060005
-			public const int item2 = 2131099653;
+			public const int item3 = 2131099653;
 			
 			// aapt resource value: 0x7f060006
-			public const int item3 = 2131099654;
-			
-			// aapt resource value: 0x7f060007
-			public const int item4 = 2131099655;
+			public const int item4 = 2131099654;
 			
 			// aapt resource value: 0x7f060001
 			public const int layout = 2131099649;
