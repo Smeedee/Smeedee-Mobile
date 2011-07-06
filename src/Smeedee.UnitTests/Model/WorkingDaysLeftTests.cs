d90e401febb4641da7c87b4440112cec9c98c7cf
Lars K. Melhus <lars.kirkholt.melhus@gmail.com>
2011-07-06 10:15:57 +0200

    Got second test working, used IBackgroundWorker to eliminate threading in the tests

--- src/Smeedee.UnitTests/Model/WorkingDaysLeftTests.cs
+++ src/Smeedee.UnitTests/Model/WorkingDaysLeftTests.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using NUnit;
 using NUnit.Framework;
 using Smeedee.Model;
 using Smeedee.Services;
+using Smeedee.Utilities;
 
 namespace Smeedee.UnitTests.Model
 {
     [TestFixture]
-    class WorkingDaysLeftTests
+    public class WorkingDaysLeftTests
     {
         [Test]
         public void Should_invoke_callback_on_load()
         {
-            SmeedeeApp.Instance.ServiceLocator.Bind<IWorkingDaysLeftService>(new WorkingDaysLeftFakeService());
+            SmeedeeApp.Instance.ServiceLocator.Bind<IWorkingDaysLeftService>(new WorkingDaysLeftFakeService(new NoBackgroundWorker()));
             var model = new WorkingDaysLeft();
 
             var callbackWasExecuted = false;
@@ -26,18 +28,32 @@ namespace Smeedee.UnitTests.Model
 
             Assert.IsTrue(callbackWasExecuted);
         }
-    }
 
-    interface IWorkingDaysLeftService
-    {
-        int GetNumberOfWorkingDaysLeft(Action<AsyncResult<int>> callback);
-    }
-
-    class WorkingDaysLeftFakeService : IWorkingDaysLeftService
-    {
-        public int GetNumberOfWorkingDaysLeft(Action<AsyncResult<int>> callback)
+        [Test]
+        public void Should_fetch_data_from_service()
         {
-            throw new NotImplementedException();
+            SmeedeeApp.Instance.ServiceLocator.Bind<IWorkingDaysLeftService>(new WorkingDaysLeftFakeService(new NoBackgroundWorker()));
+            var model = new WorkingDaysLeft();
+
+            model.Load(() => { });
+
+            Assert.AreEqual(42, model.DaysLeft);
+        }
+    }
+
+    public class WorkingDaysLeftFakeService : IWorkingDaysLeftService
+    {
+        private IBackgroundWorker worker;
+
+        public WorkingDaysLeftFakeService(IBackgroundWorker worker)
+        {
+            this.worker = worker;
+        }
+
+        public void GetNumberOfWorkingDaysLeft(Action<int> callback)
+        {
+            const int fakeResult = 42;
+            worker.Invoke(() => callback(fakeResult));
         }
     }
 
--- src/Smeedee/Model/WorkingDaysLeft.cs
+++ src/Smeedee/Model/WorkingDaysLeft.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Smeedee.Services;
 
 namespace Smeedee.Model
 {
     public class WorkingDaysLeft
     {
+        private IWorkingDaysLeftService service = SmeedeeApp.Instance.ServiceLocator.Get<IWorkingDaysLeftService>();
+
         public void Load(Action callback)
         {
-            callback();
+            service.GetNumberOfWorkingDaysLeft((result) => { 
+                DaysLeft = result;
+                callback(); 
+            }); 
         }
+
+        public int DaysLeft { get; set; }
     }
 }
