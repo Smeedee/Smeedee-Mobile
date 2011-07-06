02cb6f56199f2d39ae3af6d80b884bd57ae94949
Lars K. Melhus <lars.kirkholt.melhus@gmail.com>
2011-07-06 12:43:53 +0200

    Some refactoring. Added a check for null and exception.

--- src/Smeedee.Android/Widgets/WorkingDaysLeftWidget.cs
+++ src/Smeedee.Android/Widgets/WorkingDaysLeftWidget.cs
@@ -1,17 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-using Android.App;
 using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Util;
 using Android.Views;
 using Android.Widget;
 using Smeedee.Model;
-using Smeedee.Services;
 
 namespace Smeedee.Android.Widgets
 {
@@ -21,16 +13,22 @@ namespace Smeedee.Android.Widgets
 
         public WorkingDaysLeftWidget(Context context) : base(context)
         {
-            Initialize();
+            InitializeView();
 
             model = new WorkingDaysLeft();
             model.Load(UpdateView);
         }
 
-        private void Initialize()
+        private void InitializeView()
         {
             var inflater = Context.GetSystemService(Context.LayoutInflaterService) as LayoutInflater;
-            inflater.Inflate(Resource.Layout.WorkingDaysLeftWidget, this);
+            if (inflater != null)
+            {
+                inflater.Inflate(Resource.Layout.WorkingDaysLeftWidget, this);
+            } else
+            {
+                throw new Exception("Unable to inflate view on Working days left widget");
+            }
         }
 
         private void UpdateView()
@@ -39,6 +37,5 @@ namespace Smeedee.Android.Widgets
             var days = model.DaysLeft.ToString();
             textView.SetText(days, TextView.BufferType.Normal);
         }
-
     }
 }
