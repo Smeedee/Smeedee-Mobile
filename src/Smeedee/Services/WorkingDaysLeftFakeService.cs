2c724c7721a35471f43766ec10aa1c1750e7b903
Lars K. Melhus <lars.kirkholt.melhus@gmail.com>
2011-07-06 10:55:00 +0200

    Added another fake working days service, to be used with the UI code

---
+++ src/Smeedee/Services/WorkingDaysLeftFakeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smeedee.Services
+{
+    class WorkingDaysLeftFakeService : IWorkingDaysLeftService
+    {
+        public void GetNumberOfWorkingDaysLeft(Action<int> callback)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
