47b67b9b1508cbf4676e0ebf90f116727202e846
Lars K. Melhus <lars.kirkholt.melhus@gmail.com>
2011-07-07 14:21:46 +0200

    Added another element in the test list

--- src/Smeedee.UnitTests/Model/TopCommittersTests.cs
+++ src/Smeedee.UnitTests/Model/TopCommittersTests.cs
@@ -8,7 +8,7 @@ namespace Smeedee.UnitTests.Model
 {
     [TestFixture]
     public class TopCommittersTests
-    {   
+    {
         [Test]
         [ExpectedException(typeof(ArgumentNullException))]
         public void Should_throw_exception_on_null_argument()
@@ -26,20 +26,22 @@ namespace Smeedee.UnitTests.Model
             {
                 model = new TopCommitters(new[] {
                     new Committer("Lars", 17, "http://www.foo.com/img.png"),
-                    new Committer("Dag Olav", 19, "http://www.foo.com/img.png")
+                    new Committer("Dag Olav", 24, "http://www.foo.com/img.png"),
+                    new Committer("Borge", 16, "http://www.foo.com/img.png")
                 });
             }
 
             [Test]
             public void Should_make_accessible_list_of_committers_provided()
             {
-                Assert.AreEqual(2, model.Committers.Count());
+                Assert.AreEqual(3, model.Committers.Count());
             }
 
             [Test]
             public void Should_return_list_in_sorted_order()
             {
-                Assert.AreEqual(19, model.Committers.First().Commits);
+                Assert.AreEqual(24, model.Committers.First().Commits);
+                Assert.AreEqual(17, model.Committers.ElementAt(1).Commits);
             }
         }
     }
