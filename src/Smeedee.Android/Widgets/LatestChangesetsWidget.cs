eebe328a1b135852b369dab817e2e1062a5454ea
Lars Eidnes <larseidnes@gmail.com>
2011-07-07 12:16:48 +0200

    LatestChangeset UI polish

--- src/Smeedee.Android/Widgets/LatestChangesetsWidget.cs
+++ src/Smeedee.Android/Widgets/LatestChangesetsWidget.cs
@@ -56,11 +56,16 @@ namespace Smeedee.Android.Widgets
                 keyValueMap.Add("Image", Resource.Drawable.DefaultPerson);
                 keyValueMap.Add("User", changeset.User);
                 keyValueMap.Add("Text", changeset.Message);
-                keyValueMap.Add("Date", changeset.Date);
+                keyValueMap.Add("Date", PrettyPrint(changeset.Date));
                 listItems.Add(keyValueMap);
             }
             return listItems;
 
+        }
+
+        private string PrettyPrint(DateTime date)
+        {
+            return (DateTime.Now - date).ToHumanReadableString();
         }
     }
     public class FakeChangesetService : IChangesetService
@@ -69,12 +74,72 @@ namespace Smeedee.Android.Widgets
         {
             return new []
                 {
-                    new Changeset() { Date = DateTime.Now, User = "larspars", 
+                    new Changeset() { Date = new DateTime(2011, 7, 4, 2, 0, 0), 
+                                      User = "larspars", 
                                       Message = "Refactored HerpFactory.Derp()", },
-                    new Changeset() { Date = DateTime.Now, User = "larspars", 
+                    new Changeset() { Date = new DateTime(2011, 7, 3, 2, 0, 0), 
+                                      User = "larspars", 
                                       Message = "Fixed a lot, so this is a really long commit message. In this commit message I have also included several newlines \n\n 1) How will that look? \r\n 2) How about that? ",}
                            
                 };
+        }
+    }
+
+    public static class TimeSpanHelpers
+    {
+        public static string ToHumanReadableString(
+            this TimeSpan ts)
+        {
+            const int SECOND = 1;
+            const int MINUTE = 60 * SECOND;
+            const int HOUR = 60 * MINUTE;
+            const int DAY = 24 * HOUR;
+            const int MONTH = 30 * DAY;
+
+            var delta = ts.TotalSeconds;
+
+            if (delta < 0)
+            {
+                return "not yet";
+            }
+            if (delta < 1 * MINUTE)
+            {
+                return ts.Seconds == 1 ? "one second ago" : ts.Seconds + " seconds ago";
+            }
+            if (delta < 2 * MINUTE)
+            {
+                return "a minute ago";
+            }
+            if (delta < 45 * MINUTE)
+            {
+                return ts.Minutes + " minutes ago";
+            }
+            if (delta < 90 * MINUTE)
+            {
+                return "an hour ago";
+            }
+            if (delta < 24 * HOUR)
+            {
+                return ts.Hours + " hours ago";
+            }
+            if (delta < 48 * HOUR)
+            {
+                return "yesterday";
+            }
+            if (delta < 30 * DAY)
+            {
+                return ts.Days + " days ago";
+            }
+            if (delta < 12 * MONTH)
+            {
+                int months = Convert.ToInt32(Math.Floor((double)ts.Days / 30));
+                return months <= 1 ? "one month ago" : months + " months ago";
+            }
+            else
+            {
+                int years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
+                return years <= 1 ? "one year ago" : years + " years ago";
+            }
         }
     }
 }
