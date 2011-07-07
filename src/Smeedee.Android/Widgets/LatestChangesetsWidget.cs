835a480c76ffc142eb6da5e9017bbabd933feab0
Lars Eidnes <larseidnes@gmail.com>
2011-07-07 09:44:15 +0200

    Added some skeleton classes for the LatestChangeset widget

--- src/Smeedee.Android/Widgets/LatestChangesetsWidget.cs
+++ src/Smeedee.Android/Widgets/LatestChangesetsWidget.cs
@@ -7,14 +7,19 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Text;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using Smeedee.Model;
+using Smeedee.Services;
+using Object = Java.Lang.Object;
 
 namespace Smeedee.Android.Widgets
 {
     public class LatestChangesetsWidget : RelativeLayout, IWidget
     {
+        private IChangesetService changesetService;
         public LatestChangesetsWidget(Context context) :
             base(context)
         {
@@ -29,7 +34,52 @@ namespace Smeedee.Android.Widgets
 
         private void Initialize()
         {
+            changesetService = new FakeChangesetService();
+            var inflater = Context.GetSystemService(Context.LayoutInflaterService) as LayoutInflater;
+            inflater.Inflate(Resource.Layout.LatestChangesetsWidget, this);
 
+            var commitList = FindViewById<ListView>(Resource.Id.LatestChangesetsList);
+            //Do();
+            //var list = BuildList();
+            //AddView(BuildList());
+        }
+
+        private void Do()
+        {
+            var commitList = FindViewById<ListView>(Resource.Id.LatestChangesetsList);
+            LayoutInflater inflater = LayoutInflater.From(Context);
+            inflater.Inflate(Resource.Layout.LatestChangesetsWidget_ListItem, commitList);
+            
+        }
+
+        private ListView BuildList()
+        {
+            var commitList = FindViewById<ListView>(Resource.Id.LatestChangesetsList);
+            foreach (var changeset in changesetService.GetLatestChangesets())
+            {
+                var view = new LinearLayout(Context);
+                var img = new ImageView(Context);
+                img.SetImageResource(Resource.Drawable.DefaultPerson);
+                img.SetMaxHeight(50);
+                img.SetMaxWidth(50);
+                view.AddView(img);
+                commitList.AddView(view);
+            }
+            return commitList;
+        }
+    }
+    public class FakeChangesetService : IChangesetService
+    {
+        public IEnumerable<Changeset> GetLatestChangesets()
+        {
+            return new []
+                {
+                    new Changeset() { Date = DateTime.Now, User = "larspars", 
+                                      Message = "Refactored HerpFactory.Derp()", },
+                    new Changeset() { Date = DateTime.Now, User = "larspars", 
+                                      Message = "Fixed a lot, so this is a really long commit message. In this commit message I have also included several newlines \n\n 1) How will that look? \r\n 2) How about that? ",}
+                           
+                };
         }
     }
 }
---
+++ src/Smeedee/Model/Changeset.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smeedee.Model
+{
+    public class Changeset
+    {
+        public string Message;
+        public DateTime Date;
+        public string User;
+    }
+}
