9d7be8be6462d28e0cbbac46b27b8e1f4ffe3674
Lars K. Melhus <lars.kirkholt.melhus@gmail.com>
2011-07-07 13:52:56 +0200

    Added missing test

--- src/Smeedee.UnitTests/Model/TopCommittersTests.cs
+++ src/Smeedee.UnitTests/Model/TopCommittersTests.cs
@@ -15,5 +15,16 @@ namespace Smeedee.UnitTests.Model
         {
             var topCommiters = new TopCommitters(null);
         }
+
+        [Test]
+        public void Should_make_accessible_list_of_committers_provided()
+        {
+            var topCommitters = new TopCommitters(new[] {
+                new Committer("Lars", 17, "http://www.foo.com/img.png"),
+                new Committer("Dag Olav", 19, "http://www.foo.com/img.png")
+            });
+
+            Assert.AreEqual(2, topCommitters.Committers.Count());
+        }
     }
 }
