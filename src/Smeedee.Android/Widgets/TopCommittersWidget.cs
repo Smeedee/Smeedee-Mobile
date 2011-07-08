075a4fdc2149960b8ce3e8220853c9e6aae8eb2f
Borge Rodsjo <borge.rodsjo@gmail.com>
2011-07-08 08:41:46 +0200

    The TopBanner now contains widget name, description and the prev and next buttons. Added a paddingLeft 10 sp so that its not entirely glued to the left side of the screen. Need to come to an agreement about the layout of the TopBanner :-)

--- src/Smeedee.Android/Widgets/TopCommittersWidget.cs
+++ src/Smeedee.Android/Widgets/TopCommittersWidget.cs
@@ -16,7 +16,7 @@ using Smeedee.Services;
 namespace Smeedee.Android.Widgets
 {
     [Widget("Top Committers", Resource.Drawable.icon_topcommitters, IsEnabled = true)]
-    public class TopCommittersWidget : RelativeLayout , IWidget
+    public class TopCommittersWidget : RelativeLayout
     {
         private readonly IModelService<TopCommitters> service = SmeedeeApp.Instance.ServiceLocator.Get<IModelService<TopCommitters>>();
 
