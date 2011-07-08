d29c638926eb5971fc619efc05fc4b3337b55d35
Lars Eidnes <larseidnes@gmail.com>
2011-07-08 14:03:32 +0200

    Added Guard.NotNull(...) method to throw AgrumentNullException on nulls. Used this to validate Changeset parameters.

---
+++ src/Smeedee.UnitTests/Model/ChangesetTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Smeedee.Model;
+
+namespace Smeedee.UnitTests.Model
+{
+    [TestFixture]
+    internal class ChangesetTests
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void First_constructor_argument_can_not_be_null()
+        {
+            new Changeset(null, DateTime.Now, "larspars");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Third_constructor_argument_can_not_be_null()
+        {
+            new Changeset("msg", DateTime.Now, null);
+        }
+    }
+}
--- src/Smeedee/Model/Changeset.cs
+++ src/Smeedee/Model/Changeset.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Smeedee.Utilities;
 
 namespace Smeedee.Model
 {
     public class Changeset
     {
-        public string Message;
-        public DateTime Date;
-        public string User;
+        public string Message { get; private set; }
+        public DateTime Date { get; private set; }
+        public string User { get; private set; }
+
+        public Changeset(string message, DateTime date, string user)
+        {
+            Guard.NotNull(message, date, user);
+            User = user;
+            Date = date;
+            Message = message;
+        }
     }
 }
