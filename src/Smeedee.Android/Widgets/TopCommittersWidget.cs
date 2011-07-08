7ce92587e75f9d88d4ffbafcfca388ae5754cfd9
Lars K. Melhus <lars.kirkholt.melhus@gmail.com>
2011-07-08 09:51:07 +0200

    Finally able to dynamically load data, error was missing dimension in xml

--- src/Smeedee.Android/Widgets/TopCommittersWidget.cs
+++ src/Smeedee.Android/Widgets/TopCommittersWidget.cs
@@ -12,6 +12,7 @@ using Android.Views;
 using Android.Widget;
 using Smeedee.Model;
 using Smeedee.Services;
+using Smeedee.Utilities;
 
 namespace Smeedee.Android.Widgets
 {
@@ -19,13 +20,14 @@ namespace Smeedee.Android.Widgets
     public class TopCommittersWidget : RelativeLayout , IWidget
     {
         private readonly IModelService<TopCommitters> service = SmeedeeApp.Instance.ServiceLocator.Get<IModelService<TopCommitters>>();
+        private readonly IBackgroundWorker bgWorker = SmeedeeApp.Instance.ServiceLocator.Get<IBackgroundWorker>();
 
         private TopCommitters model;
 
         public TopCommittersWidget(Context context) : base(context)
         {
             InflateView();
-            //InitializeModelAndUpdateView();
+            bgWorker.Invoke(InitializeModelAndUpdateView);
         }
 
         private void InflateView()
@@ -43,23 +45,23 @@ namespace Smeedee.Android.Widgets
 
         private void InitializeModelAndUpdateView()
         {
-            //model = service.GetSingle();
+            model = service.GetSingle();
 
             var list = FindViewById<ListView>(Resource.Id.TopCommittersList);
+            
+            var from = new string[] { "name", "commits" };
+            var to = new int[] { Resource.Id.committer_name, Resource.Id.number_of_commits };
 
-            var from = new string[] { "Name", "Commits" };
-            var to = new int[] { Resource.Id.CommitterName, Resource.Id.NumberOfCommits };
-
-            //var committer = model.Committers.First();
+            var committer = model.Committers.First();
 
             var data = new Dictionary<string, object> {
-                {"Name", "Lars"},
-                {"Commits", "10"}
+                {"name", committer.Name},
+                {"commits", committer.Commits}
             };
             var fillData = new List<IDictionary<string, object>>() {data};
 
             var adapter = new SimpleAdapter(Context, fillData, Resource.Layout.TopCommittersWidget_ListItem, from, to);
-
+            
             Handler.Post(() => list.Adapter = adapter);
         }
     }
