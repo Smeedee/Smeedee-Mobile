8d6e249c2d0843e639df25350231f1e639b73121
Borge Rodsjo <borge.rodsjo@gmail.com>
2011-07-11 09:56:16 +0200

    Redesignet GlobalSettingsScreen to use preferences. All layout and information is staic yet.

--- src/Smeedee.Android/Screens/GlobalSettingsScreen.cs
+++ src/Smeedee.Android/Screens/GlobalSettingsScreen.cs
@@ -1,46 +1,17 @@
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Widget;
+using Android.Preferences;
 
 namespace Smeedee.Android.Screens
 {
-    [Activity(Label = "Application settings", Theme = "@android:style/Theme")]
-    public class GlobalSettings : Activity
+    [Activity(Label = "Smeedee settings", Theme = "@android:style/Theme")]
+    public class GlobalSettings : PreferenceActivity
     {
-
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            SetContentView(Resource.Layout.GlobalSettingsScreen);
 
-            BindEventsToButtons();
-        }
-
-        private void BindEventsToButtons()
-        {
-            BindClickEventToEnabledWidgetsBtn();
-            BindClickEventToServerAndUserSettingsBtn();
-        }
-
-        private void BindClickEventToEnabledWidgetsBtn()
-        {
-            var btnPrev = FindViewById<Button>(Resource.Id.BtnEnabledWidgets);
-            btnPrev.Click += (obj, e) =>
-                                 {
-                                     var enabledWidgetsScreen = new Intent(this, typeof (EnabledWidgetsScreen));
-                                     StartActivity(enabledWidgetsScreen);
-                                 };
-        }
-
-        private void BindClickEventToServerAndUserSettingsBtn()
-        {
-            var btnPrev = FindViewById<Button>(Resource.Id.BtnServerAndUserSettings);
-            btnPrev.Click += (obj, e) =>
-            {
-                var serverSettingsScreen = new Intent(this, typeof(ServerSettingsScreen));
-                StartActivity(serverSettingsScreen);
-            };
+            AddPreferencesFromResource(Resource.Layout.GlobalSettingsScreen);
         }
     }
 }
