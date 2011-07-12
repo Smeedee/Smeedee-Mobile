9beaeac225df30932a81457d5fcdcd83deab58a5
Alex York <alex-york@hotmail.co.uk>
2011-07-12 18:26:23 +0200

    Made the configuration screen a table view

--- src/Smeedee.iOS/ConfigurationScreen.xib.cs
+++ src/Smeedee.iOS/ConfigurationScreen.xib.cs
@@ -6,34 +6,59 @@ using MonoTouch.UIKit;
 
 namespace Smeedee.iOS
 {
-    public partial class ConfigurationScreen : UIViewController
+    public partial class ConfigurationScreen : UITableViewController
     {
         #region Constructors
 
-        // The IntPtr and initWithCoder constructors are required for items that need 
-        // to be able to be created from a xib rather than from managed code
-
-        public ConfigurationScreen (IntPtr handle) : base (handle)
+        public ConfigurationScreen(IntPtr handle) : base(handle)
         {
-            Initialize ();
         }
 
         [Export ("initWithCoder:")]
-        public ConfigurationScreen (NSCoder coder) : base (coder)
+        public ConfigurationScreen(NSCoder coder) : base(coder)
         {
-            Initialize ();
         }
 
-        public ConfigurationScreen () : base ("ConfigurationScreen", null)
-        {
-            Initialize ();
-        }
-
-        void Initialize ()
+        public ConfigurationScreen() : base("ConfigurationScreen", null)
         {
         }
 
         #endregion
+        
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            
+            TableView.Source = new ConfigurationTableSource();
+        }
+    }
+    
+    public class ConfigurationTableSource : UITableViewSource
+    {
+        public override int NumberOfSections(UITableView tableView)
+        {
+            return 2;
+        }
+        
+        public override string TitleForHeader(UITableView tableView, int section)
+        {
+            return "Section header " + section; 
+        }
+        
+        public override int RowsInSection(UITableView tableview, int section)
+        {
+            return 3;
+        }
+        
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("CellID") ??
+                       new UITableViewCell(UITableViewCellStyle.Subtitle, "CellID");
+            
+            cell.TextLabel.Text = "Row " + indexPath.Row;
+            cell.DetailTextLabel.Text = "Section " + indexPath.Section;
+            
+            return cell;
+        }
     }
 }
-
