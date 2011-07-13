80608ba8ebad9aa681ef041ab4fcf04efbb5f97e
Lars Eidnes <larseidnes@gmail.com>
2011-07-13 09:39:30 +0200

    Moved FakeLatestChangesetsService to Service folder

--- src/Smeedee.Android/Widgets/LatestChangesetsWidget.cs
+++ src/Smeedee.Android/Widgets/LatestChangesetsWidget.cs
@@ -123,25 +123,4 @@ namespace Smeedee.Android.Widgets
             return view;
         }
     }
-
-    public class FakeChangesetService : IModelService<Changeset>
-    {
-        public IEnumerable<Changeset> Get(IDictionary<string, string> args)
-        {
-            return new[]
-                {
-                    new Changeset("Refactored HerpFactory.Derp()", new DateTime(2011, 7, 7, 12, 0, 0), "larmel"),
-                    new Changeset("Fixed a lot, so this is a really long commit message. In this commit message I have also included several newlines \n\n 1) How will that look? \r\n 2) Should we shorten it? ", new DateTime(2011, 7, 7, 1, 10, 0), "larmel"),
-                    new Changeset("", new DateTime(2011, 7, 6, 2, 0, 0), "larspars"),
-                    new Changeset("Coded new codes.", new DateTime(2011, 7, 6, 1, 0, 0), "dagolap"),
-                    new Changeset("Programmed them programs.", new DateTime(2011, 7, 5, 1, 0, 0), "rodsjo"),
-                    new Changeset("Blabla", new DateTime(2011, 7, 7, 2, 0, 0), "larspars")
-                };
-        }
-
-        public Changeset GetSingle(IDictionary<string, string> args)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
