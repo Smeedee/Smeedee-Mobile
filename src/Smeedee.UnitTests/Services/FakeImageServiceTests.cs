c050d27e74a6b3333eb3cfb20a4531bedd8fc57d
Dag Olav Prestegarden <dagolav@prestegarden.com>
2011-07-19 14:17:04 +0200

    Removing some unused imports

--- src/Smeedee.UnitTests/Services/FakeImageServiceTests.cs
+++ src/Smeedee.UnitTests/Services/FakeImageServiceTests.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using Smeedee;
 using NUnit.Framework;
 using Smeedee.UnitTests.Fakes;
 
