5cf353c88b02feb9275ad21ec771dcf1326b5057
Lars K. Melhus <lars.kirkholt.melhus@gmail.com>
2011-07-19 11:11:42 +0200

    Changed usage to Description property, better name

--- src/Smeedee.Android/Widgets/TopCommittersWidget.cs
+++ src/Smeedee.Android/Widgets/TopCommittersWidget.cs
@@ -60,7 +60,7 @@ namespace Smeedee.Android.Widgets
 
         private void UpdateView()
         {
-            _dynamicDescription = model.DaysText;
+            _dynamicDescription = model.Description;
             list.Adapter = CreateAdapter();
         }
 
