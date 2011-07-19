62cfe9332a6987313451b51d9af10e49110883a5
Dag Olav Prestegarden <dagolav@prestegarden.com>
2011-07-19 15:13:24 +0200

    Adding tests that ensures settings are saved with correct keys

--- src/Smeedee.UnitTests/Model/BuildStatusTests.cs
+++ src/Smeedee.UnitTests/Model/BuildStatusTests.cs
@@ -9,12 +9,13 @@ namespace Smeedee.UnitTests.Model
     public class BuildStatusTests
     {
         protected SmeedeeApp smeedeeApp = SmeedeeApp.Instance;
+        private FakePersistenceService persister = new FakePersistenceService();
 
         [SetUp]
         public void SetUp()
         {
             smeedeeApp.ServiceLocator.Bind<IBuildStatusService>(new FakeBuildStatusService(new NoBackgroundInvokation()));
-            smeedeeApp.ServiceLocator.Bind<IPersistenceService>(new FakePersistenceService());
+            smeedeeApp.ServiceLocator.Bind<IPersistenceService>(persister);
         }
 
         [TestFixture]
@@ -63,10 +64,10 @@ namespace Smeedee.UnitTests.Model
             [Test]
             public void Should_properly_order_builds_alphabetically()
             {
-                var model = new BuildStatus {BrokenBuildsAtTop = false, Ordering = BuildOrder.BuildName};
+                var model = GetNameSortedModel();
                 model.Load(() => Assert.AreEqual(model.Builds.OrderBy(b => b.ProjectName), model.Builds));
             }
-
+            
             [Test]
             [Ignore] // TODO: Why is this stopping?
             public void Should_properly_order_builds_by_build_time()
@@ -78,29 +79,79 @@ namespace Smeedee.UnitTests.Model
             [Test]
             public void Should_properly_put_broken_builds_at_top()
             {
-                var model = new BuildStatus {BrokenBuildsAtTop = true, Ordering = BuildOrder.BuildName};
+                var model = GetNameSortedModelWithBrokenAtTop();
                 model.Load(() => Assert.True(model.Builds.Take(2).Where(b => b.BuildSuccessState == BuildState.Broken).Count() == 2));
             }
 
             [Test]
             public void Should_maintain_alphabetical_ordering_among_broken_builds_when_they_are_on_top()
             {
-                var model = new BuildStatus {BrokenBuildsAtTop = true, Ordering = BuildOrder.BuildName};
+                var model = GetNameSortedModelWithBrokenAtTop();
                 model.Load(() => Assert.AreEqual(model.Builds.Take(2).OrderBy(b => b.ProjectName), model.Builds.Take(2)));
             }
 
             [Test]
             public void Should_maintain_alphabetical_ordering_among_working_builds_when_broken_builds_are_on_top()
             {
-                var model = new BuildStatus {BrokenBuildsAtTop = true, Ordering = BuildOrder.BuildName};
+                var model = GetNameSortedModelWithBrokenAtTop();
                 model.Load(() => Assert.AreEqual(model.Builds.Skip(2).Take(3).OrderBy(b => b.ProjectName), model.Builds.Skip(2).Take(3)));
             }
 
             [Test]
             public void Should_maintain_alphabetical_ordering_among_unknown_builds_when_broken_builds_are_on_top()
             {
-                var model = new BuildStatus { BrokenBuildsAtTop = true, Ordering = BuildOrder.BuildName };
+                var model = GetNameSortedModelWithBrokenAtTop();
                 model.Load(() => Assert.AreEqual(model.Builds.Skip(5).Take(3).OrderBy(b => b.ProjectName), model.Builds.Skip(5).Take(3)));
+            }
+
+            private static BuildStatus GetNameSortedModel()
+            {
+                return new BuildStatus { BrokenBuildsAtTop = false, Ordering = BuildOrder.BuildName };
+            }
+
+            private static BuildStatus GetNameSortedModelWithBrokenAtTop()
+            {
+                return new BuildStatus { BrokenBuildsAtTop = true, Ordering = BuildOrder.BuildName };
+            }
+        }
+
+        [TestFixture]
+        public class When_saving_preferences : BuildStatusTests
+        {
+            [Test]
+            public void Should_persist_broken_first_preference_as_bool_with_correct_key()
+            {
+                var model = new BuildStatus();
+                model.BrokenBuildsAtTop = true;
+
+                Assert.AreEqual(persister.Get("brokenBuildsAtTop", false), true);
+            }
+
+            [Test]
+            public void Should_persist_sort_order_as_string_with_correct_key()
+            {
+                var model = new BuildStatus();
+                model.Ordering = BuildOrder.BuildName;
+
+                Assert.AreNotEqual("foo", persister.Get("buildSortOrdering", "foo"));
+            }
+
+            [Test]
+            public void Should_persist_sort_order_by_name_with_correct_value()
+            {
+                var model = new BuildStatus();
+                model.Ordering = BuildOrder.BuildName;
+
+                Assert.AreEqual("buildname", persister.Get("buildSortOrdering", "foo"));
+            }
+
+            [Test]
+            public void Should_persist_sort_order_by_datetime_with_correct_valye()
+            {
+                var model = new BuildStatus();
+                model.Ordering = BuildOrder.BuildTime;
+
+                Assert.AreEqual("buildtime", persister.Get("buildSortOrdering", "foo"));
             }
         }
     }
