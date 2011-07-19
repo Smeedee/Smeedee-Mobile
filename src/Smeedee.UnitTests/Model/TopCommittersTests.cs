75f53c6d513c9886fd6ce4aa2f2adb243852f206
Lars K. Melhus <lars.kirkholt.melhus@gmail.com>
2011-07-19 09:43:11 +0200

    Removed unused code, and added setup method

--- src/Smeedee.UnitTests/Model/TopCommittersTests.cs
+++ src/Smeedee.UnitTests/Model/TopCommittersTests.cs
@@ -10,86 +10,40 @@ namespace Smeedee.UnitTests.Model
     [TestFixture]
     public class TopCommittersTests
     {
+        private TopCommitters model;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var serviceLocator = SmeedeeApp.Instance.ServiceLocator;
+            serviceLocator.Bind<ITopCommittersService>(new TopCommittersFakeService());
+            
+            model = new TopCommitters();
+        }
 
         [Test]
         public void Should_initially_have_empty_list_of_committers()
         {
-            var tc = new TopCommitters();
-
-            Assert.AreEqual(0, tc.Committers.Count());
+            Assert.AreEqual(0, model.Committers.Count());
         }
-
 
         [Test]
         public void Should_run_callback_on_load()
         {
-            var tc = new TopCommitters();
             var check = false;
-            tc.Load(() => check = true);
+            model.Load(() => check = true);
 
             Assert.IsTrue(check);
         }
-
-
-
-
         /*
-
-        private Committer[] singleCommitter = {
-            new Committer("Lars", 17, "http://www.foo.com/img.png")
-        };
-
         [Test]
-        public void Should_implement_IModel()
+        public void Should_have_list_of_committers_after_load()
         {
-            Assert.That(typeof(IModel).IsAssignableFrom(typeof(TopCommitters)));
-        }
+            var tc = new TopCommitters();
+            tc.Load(() => { });
 
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void Should_throw_exception_on_null_argument()
-        {
-            new TopCommitters(null, 0);
-        }
-
-        [TestCase(1, "Top committers for the past 24 hours")]
-        [TestCase(2, "Top committers for the past 2 days")]
-        [TestCase(7, "Top committers for the past week")]
-        [TestCase(30, "Top committers for the past month")]
-        public void Should_return_string_description_of_time_period(int days, string expected)
-        {
-            var model = new TopCommitters(singleCommitter, days);
-
-            Assert.AreEqual(expected, model.DaysText);
-        }
-
-        [TestFixture]
-        public class When_instanciating_top_committers_with_list_of_committers
-        {
-            private TopCommitters model;
-
-            [SetUp]
-            public void SetUp()
-            {
-                model = new TopCommitters(new[] {
-                    new Committer("Lars", 17, "http://www.foo.com/img.png"),
-                    new Committer("Dag Olav", 24, "http://www.foo.com/img.png"),
-                    new Committer("Borge", 16, "http://www.foo.com/img.png")
-                }, 7);
-            }
-
-            [Test]
-            public void Should_make_accessible_list_of_committers_provided()
-            {
-                Assert.AreEqual(3, model.Committers.Count());
-            }
-
-            [Test]
-            public void Should_return_list_in_sorted_order()
-            {
-                Assert.AreEqual(24, model.Committers.First().Commits);
-                Assert.AreEqual(17, model.Committers.ElementAt(1).Commits);
-            }
+            Assert.AreEqual(2, tc.Committers.Count());
         }*/
+
     }
 }
