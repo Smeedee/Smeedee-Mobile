b527a44f87a996b9227bc031ff45eab3c80882b1
SoC2011 Shared MacBook <dagolav@prestegarden.com>
2011-07-21 14:46:03 +0200

    Refactored config screen a bit, minimized it

--- src/Smeedee.iOS/ConfigurationScreen.xib.cs
+++ src/Smeedee.iOS/ConfigurationScreen.xib.cs
@@ -1,38 +1,16 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using MonoTouch.Foundation;
 using MonoTouch.UIKit;
-using Smeedee.Model;
 
 namespace Smeedee.iOS
 {
     public partial class ConfigurationScreen : UINavigationController
     {
-		
-        #region Constructors
+        public ConfigurationScreen(IntPtr handle) : base(handle) { }
 
-        public ConfigurationScreen(IntPtr handle) : base(handle)
-        {
-        }
-
-        [Export ("initWithCoder:")]
-        public ConfigurationScreen(NSCoder coder) : base(coder)
-        {
-        }
-
-        public ConfigurationScreen() : base("ConfigurationScreen", null)
-        {
-        }
-
-        #endregion
-        
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-			
-			var controller = new MainConfigTableViewController();
-			PushViewController(controller, false);
+			PushViewController(new MainConfigTableViewController(), false);
         }
     }
 }
