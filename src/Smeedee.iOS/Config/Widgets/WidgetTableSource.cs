4d4f1f37b7866657418900857c6d2ef1a369b383
SoC2011 Shared MacBook <dagolav@prestegarden.com>
2011-07-21 13:10:17 +0200

    Implemented usage of WidgetModel.Enabled in the shared table source, and did some refactoring.

--- src/Smeedee.iOS/Config/Widgets/WidgetTableSource.cs
+++ src/Smeedee.iOS/Config/Widgets/WidgetTableSource.cs
@@ -7,14 +7,16 @@ using Smeedee.Model;
 
 namespace Smeedee.iOS
 {
+	/*
+	 * Intended base class intended for all widget configuration table sources.
+	 * Contains a UISwitch to toggle between enabled and disabled.
+	 */
 	public class WidgetTableSource : UITableViewSource
 	{
 		// Needs to be declared here to avoid GC issues
 		// See http://stackoverflow.com/questions/6156165/why-does-my-uiswitch-crash-when-it-is-a-tableview-cell-accessoryview
-		//
 		private UISwitch enabledSwitch;
 		
-		// Changes to the enabled state goes through this model
 		protected WidgetModel widgetModel;
 		
 		public WidgetTableSource(WidgetModel model)
@@ -24,42 +26,23 @@ namespace Smeedee.iOS
 		
 		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
 		{
-			var cell = new UITableViewCell(UITableViewCellStyle.Default, "ENABLE_WIDGET");
-			cell.TextLabel.Text = "Enabled";
-			
-			// Not nice at all :(
-			//var widgetAttributes = typeof(T).GetCustomAttributes(typeof(WidgetAttribute), true);
-			//var enabledKey = (widgetAttributes.First() as WidgetAttribute).Name;
-			//var isEnabled = persister.Get(enabledKey, false);
-			
 			enabledSwitch = new UISwitch();
-			enabledSwitch.SetState(true, false); // widgetModel.Enabled
-			
+			enabledSwitch.SetState(widgetModel.Enabled, false);
 			enabledSwitch.ValueChanged += delegate {
-				// Want to do this: 
-				//widgetModel.Enabled = enabledSwitch.On;
-				
-				//persister.Save(enabledKey, enabledSwitch.On);
+				widgetModel.Enabled = enabledSwitch.On;
 			};
 			
+			var cell = new UITableViewCell(UITableViewCellStyle.Default, "EnableWidgetUISwitch");
 			cell.AccessoryView = enabledSwitch;
+			cell.TextLabel.Text = "Enabled";
 			return cell;
 		}
 		
-        public override int NumberOfSections(UITableView tableView)
-        {
-            return 1;
-        }
+        public override int NumberOfSections(UITableView tableView) { return 1; }
 		
-        public override int RowsInSection(UITableView tableview, int section)
-        {
-			return 1;
-        }
+        public override int RowsInSection(UITableView tableview, int section) { return 1; }
         
-        public override string TitleForHeader(UITableView tableView, int section)
-        {
-			return "General";
-        }
+        public override string TitleForHeader(UITableView tableView, int section) { return "General"; }
 	}
 }
 
