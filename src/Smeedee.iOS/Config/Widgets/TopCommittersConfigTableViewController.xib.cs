613d8e56fe164166cf30d3a68677a25c08f7c71a
SoC2011 Shared MacBook <dagolav@prestegarden.com>
2011-07-21 14:16:33 +0200

    Deleted comment, and parameter to table view source. Can be added later if the settings view needs to spawn new windows

--- src/Smeedee.iOS/Config/Widgets/TopCommittersConfigTableViewController.xib.cs
+++ src/Smeedee.iOS/Config/Widgets/TopCommittersConfigTableViewController.xib.cs
@@ -7,27 +7,23 @@ using Smeedee.Model;
 
 namespace Smeedee.iOS
 {
-	// TODO: Rename to TopCommittersConfigTableViewController
 	public partial class TopCommittersConfigTableViewController : UITableViewController
 	{
 		public TopCommittersConfigTableViewController() : base("TopCommittersWidgetTableViewController", null)
 		{
 			this.Title = "Top Committers";
-			this.TableView.Source = new TopCommittersConfigTableSource(this);
+			this.TableView.Source = new TopCommittersConfigTableSource();
 		}
 	}
 	
 	public class TopCommittersConfigTableSource : WidgetConfigTableSource
 	{
-		private TopCommittersConfigTableViewController controller;
-		
-		public TopCommittersConfigTableSource(TopCommittersConfigTableViewController controller) 
+		public TopCommittersConfigTableSource() 
 			: base(SmeedeeApp.Instance.AvailableWidgets.Where(e => e.SettingsType == typeof(TopCommittersConfigTableViewController)).First())
 		{
-			this.controller = controller;
+			
 		}
 		
 		// TODO: The rest of the input controllers
-		
 	}
 }
