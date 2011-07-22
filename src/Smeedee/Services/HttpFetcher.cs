71e4ef2a2532b1026f4dbb2dedfd82a5d07caf65
Dag Olav Prestegarden <dagolav@prestegarden.com>
2011-07-22 13:02:34 +0200

    Fixing broken link to renamed httpfetcher, adding integration test for httpfetcher that dictates an empty string be returned if we encounter errors fetching data from web

--- src/Smeedee/Services/HttpFetcher.cs
+++ src/Smeedee/Services/HttpFetcher.cs
@@ -1,17 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
 
 namespace Smeedee.Services
 {
-    class HttpFetcher : IFetchHttp
+    public class HttpFetcher : IFetchHttp
     {
         public string DownloadString(string url)
         {
             var client = new WebClient();
-            return client.DownloadString(url);
+            try
+            {
+                return client.DownloadString(url);
+            }
+            catch (Exception)
+            {
+                return "";
+            }
         }
     }
 }
