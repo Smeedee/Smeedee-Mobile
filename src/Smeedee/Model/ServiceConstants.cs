08364c76ee32ae28a6123a73ad5406a089693df0
Dag Olav Prestegarden <dagolav@prestegarden.com>
2011-07-22 14:00:20 +0200

    Adding new serviceconstants file  i referenced in last commit

---
+++ src/Smeedee/Model/ServiceConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smeedee.Model
+{
+    public static class ServiceConstants
+    {
+        public const string MOBILE_SERVICES_RELATIVE_PATH = "MobileServices";
+
+        public const string BUILD_STATUS_SERVICE_URL = "/BuildStatus/";
+    }
+}
