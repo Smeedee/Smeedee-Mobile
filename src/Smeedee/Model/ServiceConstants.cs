fab95199c06653cdddafb6459b8079a221746ae3
Borge Rodsjo <borge.rodsjo@gmail.com>
2011-07-25 08:16:17 +0200

    Removed unused usings

--- src/Smeedee/Model/ServiceConstants.cs
+++ src/Smeedee/Model/ServiceConstants.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Smeedee.Model
+﻿namespace Smeedee.Model
 {
     public static class ServiceConstants
     {
