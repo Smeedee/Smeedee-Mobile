24c98da399ccf6dddd6a8ac6ce67f2cd59bbc0fe
SoC2011 Shared MacBook <dagolav@prestegarden.com>
2011-07-25 14:31:54 +0200

    Changed build ordering in Build Status widget when ordering on time. The ordering was reversed. Now it is correct

--- src/Smeedee/Services/Fakes/FakeBuildStatusService.cs
+++ src/Smeedee/Services/Fakes/FakeBuildStatusService.cs
@@ -16,11 +16,11 @@ namespace Smeedee
             this.bgWorker = bgWorker;
             builds = new List<Build>()
                          {
-                            new Build("Smeedee-Mobile - Master", BuildState.Working, "dagolap", DateTime.Now),
-                            new Build("Smeedee-Mobile - Dev", BuildState.Broken, "dagolap", DateTime.Now),
-                            new Build("Smeedee - Master", BuildState.Working, "stavanger", DateTime.Now), 
-                            new Build("Smeedee - Dev", BuildState.Unknown, "stavanger", DateTime.Now),
-                            new Build("SharedMobileTest1", BuildState.Unknown, "trondheim", DateTime.Now),
+                            new Build("ASmeedee-Mobile - Master", BuildState.Working, "dagolap", new DateTime(2010, 1, 1, 1, 1, 1)),
+                            new Build("BSmeedee-Mobile - Dev", BuildState.Broken, "dagolap", new DateTime(2009, 1, 1, 1, 1, 1)),
+                            new Build("CSmeedee - Master", BuildState.Working, "stavanger", new DateTime(2008, 1, 1, 1, 1, 1)), 
+                            new Build("DSmeedee - Dev", BuildState.Unknown, "stavanger", new DateTime(2007, 1, 1, 1, 1, 1)),
+                            new Build("SharedMobileTest1", BuildState.Unknown, "trondheim", new DateTime(2006, 1, 1, 1, 1, 1)),
                             new Build("SharedMobileTest2", BuildState.Unknown, "trondheim", DateTime.Now),
                             new Build("Smeedee Webpage", BuildState.Working, "stavanger", DateTime.Now),
                             new Build("Alex Project", BuildState.Broken, "alex", DateTime.Now)
