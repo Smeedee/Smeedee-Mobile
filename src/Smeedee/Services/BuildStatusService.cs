982f613c69fd406a4d91ba5504438913c7b07b96
Lars Eidnes <larseidnes@gmail.com>
2011-07-26 11:01:29 +0200

    Removed url normalization and use of persistor in BuildStatusServices (now uses Login model)

--- src/Smeedee/Services/BuildStatusService.cs
+++ src/Smeedee/Services/BuildStatusService.cs
@@ -14,23 +14,16 @@ namespace Smeedee.Services
 
         private readonly IFetchHttp downloader;
         private readonly IBackgroundWorker bgWorker;
-        private readonly IPersistenceService persistenceService;
 
         public BuildStatusService()
         {
             downloader = SmeedeeApp.Instance.ServiceLocator.Get<IFetchHttp>();
             bgWorker = SmeedeeApp.Instance.ServiceLocator.Get<IBackgroundWorker>();
-            persistenceService = SmeedeeApp.Instance.ServiceLocator.Get<IPersistenceService>();
         }
 
         public void Load(Action<AsyncResult<IEnumerable<Build>>> callback)
         {
-            bgWorker.Invoke(() =>
-                                {
-                                    var url = persistenceService.Get(Login.LoginUrl, "http://services.smeedee.org/smeedee/");
-                                    if (!url.EndsWith("/")) url += "/";
-                                    callback(new AsyncResult<IEnumerable<Build>>(ParseCsv(downloader.DownloadString(url + ServiceConstants.MOBILE_SERVICES_RELATIVE_PATH + ServiceConstants.BUILD_STATUS_SERVICE_URL))));
-                                });
+            bgWorker.Invoke(() => callback(new AsyncResult<IEnumerable<Build>>(ParseCsv(downloader.DownloadString(new Login().Url + ServiceConstants.MOBILE_SERVICES_RELATIVE_PATH + ServiceConstants.BUILD_STATUS_SERVICE_URL)))));
         }
 
         private static IEnumerable<Build> ParseCsv(string csvData)
