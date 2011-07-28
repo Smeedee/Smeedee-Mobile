1918ffebf9dacf9985307155e4cd5f70ce0726a4
Lars Eidnes <larseidnes@gmail.com>
2011-07-28 10:29:31 +0200

    Whoops, missed the test files for last commit

---
+++ src/Smeedee.UnitTests/Services/ValidationServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Smeedee.Model;
+using Smeedee.Services;
+using Smeedee.UnitTests.Fakes;
+
+namespace Smeedee.UnitTests.Services
+{
+    [TestFixture]
+    public class ValidationServiceTests
+    {
+        private FakeHttpFetcher fakeHttpFetcher = new FakeHttpFetcher("");
+        private SmeedeeApp app = SmeedeeApp.Instance;
+        private ValidationService validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            app.ServiceLocator.Bind<IBackgroundWorker>(new NoBackgroundInvocation());
+            app.ServiceLocator.Bind<IFetchHttp>(fakeHttpFetcher);
+            validator = new ValidationService();
+        }
+
+        [Test]
+        public void Should_return_false_when_web_call_returns_false()
+        {
+            fakeHttpFetcher.SetHtmlString("False");
+            var wasValid = true;
+            validator.Validate("http://example.com", "", valid => wasValid = valid);
+            Assert.False(wasValid);
+        }
+
+        [TestCase("")]
+        [TestCase("notTrue")]
+        [TestCase("failed")]
+        public void Should_return_false_when_web_call_returns_anything_other_than_true(string key)
+        {
+            fakeHttpFetcher.SetHtmlString("");
+            var wasValid = true;
+            validator.Validate("http://example.com", key, valid => wasValid = valid);
+            Assert.False(wasValid);
+        }
+
+        [Test]
+        public void Should_return_true_when_web_call_returns_true()
+        {
+            fakeHttpFetcher.SetHtmlString("True");
+            var wasValid = false;
+            validator.Validate("http://example.com", "", valid => wasValid = valid);
+            Assert.True(wasValid);
+        }
+
+        [Test]
+        public void Should_make_call_to_the_given_url()
+        {
+            var url = "http://example.com";
+            validator.Validate(url, "key123", valid => { });
+            var expectedUrl = url + ServiceConstants.MOBILE_SERVICES_RELATIVE_PATH +
+                              ServiceConstants.VALIDATION_SERVICE_URL + "?apiKey=key123";
+            Assert.AreEqual(expectedUrl, fakeHttpFetcher.UrlAskedFor);
+        }
+    }
+}
---
+++ src/Smeedee/Services/ValidationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Smeedee.Model;
+
+namespace Smeedee.Services
+{
+    public class ValidationService : IValidationService
+    {
+        private IFetchHttp http;
+        private IBackgroundWorker bgWorker;
+
+        public ValidationService()
+        {
+            http = SmeedeeApp.Instance.ServiceLocator.Get<IFetchHttp>();
+            bgWorker = SmeedeeApp.Instance.ServiceLocator.Get<IBackgroundWorker>();
+        }
+
+        public void Validate(string url, string key, Action<bool> callback)
+        {
+            bgWorker.Invoke(() =>
+                                {
+                                    url += ServiceConstants.MOBILE_SERVICES_RELATIVE_PATH +
+                                           ServiceConstants.VALIDATION_SERVICE_URL +
+                                           "?apiKey=" + key;
+                                    var result = http.DownloadString(url);
+                                    callback(result == "True");
+                                });
+        }
+    }
+}
