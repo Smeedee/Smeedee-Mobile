b4c11ee69bc20fbe37a89a70b837246df516aaff
SoC2011 Shared MacBook <dagolav@prestegarden.com>
2011-07-29 10:48:16 +0200

    Added new interface to be used for widgets with integrated configuration in toolbar

---
+++ src/Smeedee.iOS/Model/IToolbarControl.cs
@@ -0,0 +1,12 @@
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace Smeedee.iOS
+{
+	public interface IToolbarControl
+	{
+		UIBarButtonItem ToolbarConfigurationItem();
+	}
+}
+
