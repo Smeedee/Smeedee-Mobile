4e539f6cead818804ec3bf12a4df0d91d723748a
Lars Eidnes <larseidnes@gmail.com>
2011-08-02 12:13:46 +0200

    Ported HttpFetcher to WP7

--- src/Smeedee/Services/HttpFetcher.cs
+++ src/Smeedee/Services/HttpFetcher.cs
@@ -1,21 +1,40 @@
 ﻿using System;
 using System.Net;
+using System.Threading;
 
 namespace Smeedee.Services
 {
     public class HttpFetcher : IFetchHttp
     {
+        /*
+         * What we do here is to do force an async WebClient call to be sync.
+         * This is for historical reasons. This was originally a synchronous api,
+         * (but meant to be called in a background thread).
+         * But WP7 doesn't support the sync api, so we have to force the async call to be synchronous.
+         * 
+         * In summary: We take an async api, make it sync, then fire up a thread to use it asynchronously.
+         * And this dance is intentional:)         
+         */
+        static ManualResetEvent manualReset = new ManualResetEvent(false);
+        private readonly TimeSpan TIMEOUT = TimeSpan.FromSeconds(15);
+
         public string DownloadString(string url)
         {
+            var manualReset = new ManualResetEvent(false);
             var client = new WebClient();
-            try
+
+            var result = "";
+            client.DownloadStringCompleted += (o, e) => 
             {
-                return client.DownloadString(url);
-            }
-            catch (Exception e)
-            {
-                return "";
-            }
+                if (e.Error == null)
+                    result = e.Result;
+                manualReset.Set();
+            };
+            client.DownloadStringAsync(new Uri(url));
+
+            manualReset.WaitOne(TIMEOUT);
+
+            return result;
         }
     }
 }
