68760d8686009bb22ec0848d177898ea9b782ab5
Borge Rodsjo <borge.rodsjo@gmail.com>
2011-08-03 12:47:21 +0200

    Removed unused usings

--- src/Smeedee.WP7/Converters/UriToImageConverter.cs
+++ src/Smeedee.WP7/Converters/UriToImageConverter.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
+
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using Smeedee.Model;
 
 namespace Smeedee.WP7.Converters
