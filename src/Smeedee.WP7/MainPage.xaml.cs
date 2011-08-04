818db64573b510784dae45e98841b9b154f9418a
Borge Rodsjo <borge.rodsjo@gmail.com>
2011-08-04 15:04:13 +0200

    Removed some commented code in MainPage.xaml.cs

--- src/Smeedee.WP7/MainPage.xaml.cs
+++ src/Smeedee.WP7/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using Smeedee.WP7.Widgets;
 
 namespace Smeedee.WP7
@@ -21,55 +22,6 @@ namespace Smeedee.WP7
             if (!App.ViewModel.IsDataLoaded)
                 App.ViewModel.LoadData();
         }
-
-        //private void list_LayoutUpdated(object sender, System.EventArgs e)
-        //{
-        //    var itemContainerGenerator = list.ItemContainerGenerator;
-        //    if (itemContainerGenerator == null) return;
-
-        //    if (list.ItemContainerGenerator == null) return;
-        //    for (var i = 0; i < VisualTreeHelper.GetChildrenCount(list); i++)
-        //    {
-        //        var listBoxItem = list.ItemContainerGenerator.ContainerFromIndex(i) as ListBoxItem;
-        //        if (listBoxItem == null) return;
-        //        var progressBar1 = FindFirstElementInVisualTree<ProgressBar>(listBoxItem);
-        //    }
-
-            
-
-        //    if (progressBar1 == null) return;
-        //    progressBar1.Minimum = 0;
-        //    progressBar1.Maximum = 100;
-        //    progressBar1.Value = 39;
-        //}
-        ///*
-        // * This method can be used to find the first element of a specific type in a parent element in the MainPage xaml.
-        // * For instance we use this to find a ProgressBar inside a ListBoxItem. Just pass in the parent element, and 
-        // * the element type you are searching for and you'll recieve it back.
-        // */
-        //private static T FindFirstElementInVisualTree<T>(DependencyObject parentElement) where T : DependencyObject
-        //{
-        //    var count = VisualTreeHelper.GetChildrenCount(parentElement);
-        //    if (count == 0)
-        //        return null;
-
-        //    for (int i = 0; i < count; i++)
-        //    {
-        //        var child = VisualTreeHelper.GetChild(parentElement, i);
-
-        //        if (child != null && child is T)
-        //        {
-        //            return (T)child;
-        //        }
-        //        else
-        //        {
-        //            var result = FindFirstElementInVisualTree<T>(child);
-        //            if (result != null)
-        //                return result;
-        //        }
-        //    }
-        //    return null;
-        //}
 
         private void SettingsIcon_Click(object sender, EventArgs e)
         {
