4653aa11ab93cc2610edc3e73f2b06df54fe83e1
SoC2011 Shared MacBook <dagolav@prestegarden.com>
2011-08-09 13:47:24 +0200

    Fixed width of connect dialog

--- src/Smeedee.iOS/AppDelegateShared.cs
+++ src/Smeedee.iOS/AppDelegateShared.cs
@@ -93,8 +93,8 @@ namespace Smeedee.iOS
 		{
 			if (!previousWasServerConfig) {
 				previousWasServerConfig = true;
-				loginHeaderController.View.Frame = new RectangleF(0, 0, 320, 150);
-				loginController.View.Frame = new RectangleF(0, 150, 320, 460);
+				loginHeaderController.View.Frame = new RectangleF(0, 0, Platform.ScreenWidth, 150);
+				loginController.View.Frame = new RectangleF(0, 150, Platform.ScreenWidth, Platform.ScreenHeight - 150);
 				
 				(loginController as ServerConfigTableViewController).LoginAction = ServerCallback;
 				
