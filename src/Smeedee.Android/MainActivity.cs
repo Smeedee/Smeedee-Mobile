7c574a34b17a2f4c395103bf6509af5b60715dcc
Lars Eidnes <larseidnes@gmail.com>
2011-08-10 08:59:25 +0200

    Enabled real services on android

--- src/Smeedee.Android/MainActivity.cs
+++ src/Smeedee.Android/MainActivity.cs
@@ -17,7 +17,7 @@ namespace Smeedee.Android
     public class SmeedeeApplication : Application
     {
         public SmeedeeApp App { get; private set; }
-        private const bool USE_FAKES = true;
+        private const bool USE_FAKES = false;
 
         public SmeedeeApplication(IntPtr handle) : base(handle)
         {
