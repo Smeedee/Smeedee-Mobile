253fa2a24cfa630f8d15ba1c5a775c9c454187cd
SoC2011 Shared MacBook <dagolav@prestegarden.com>
2011-08-10 14:03:54 +0200

    Changed position of Widgets headline in settings view, and changed color to orange

--- src/Smeedee.iOS/Config/Views/ConfigTableSectionHeader.cs
+++ src/Smeedee.iOS/Config/Views/ConfigTableSectionHeader.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using MonoTouch.UIKit;
 using MonoTouch.Foundation;
 using System.Drawing;
+using Smeedee.iOS;
 using Smeedee.iOS.Lib;
 
 namespace Smeedee.iOS
 {
 	public class ConfigTableSectionHeader : UIView
 	{
-		private static int padding = 15;
+		private static int padding = Platform.Name == Device.Iphone ? 15 : 45;
 		public static int Height = 35;
 		
 		private UILabel label;
@@ -33,7 +34,7 @@ namespace Smeedee.iOS
 			label.Text = header;
 			label.BackgroundColor = StyleExtensions.transparent;
 			label.Font = UIFont.BoldSystemFontOfSize(17);
-			label.TextColor = StyleExtensions.darkGrayHeadline;
+			label.TextColor = StyleExtensions.smeedeeOrange; //StyleExtensions.darkGrayHeadline;
 		}
 	}
 }
